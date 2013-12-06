a7f0a6083a17e7e837eb6c5fd891f9282e793cb0
Asari Mitsuteru <asari.mtr@gmail.com>
2013-12-06 20:14:22 +0900

    Initial Commit

---
+++ Benkyoukai/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Benkyoukai
+//
+//  Created by 浅利 光輝 on 2013/12/06.
+//  Copyright (c) 2013年 浅利 光輝. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
