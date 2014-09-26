163d41467ab68d1fc32f69f95d20d9625b120feb
Asari Mitsuteru <asari.mtr@gmail.com>
2014-09-27 08:26:43 +0900

    add class

---
+++ Benkyoukai/Cipher.h
@@ -0,0 +1,13 @@
+//
+//  Cipher.h
+//  Benkyoukai
+//
+//  Created by 浅利 光輝 on 2014/04/10.
+//  Copyright (c) 2014年 浅利 光輝. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cipher : NSObject
+
+@end
---
+++ Benkyoukai/Cipher.m
@@ -0,0 +1,13 @@
+//
+//  Cipher.m
+//  Benkyoukai
+//
+//  Created by 浅利 光輝 on 2014/04/10.
+//  Copyright (c) 2014年 浅利 光輝. All rights reserved.
+//
+
+#import "Cipher.h"
+
+@implementation Cipher
+
+@end
